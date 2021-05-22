ee3ddac1cc40ab328992589708b89ecc263a95a1
Madan-444 <mmadan3600@gmail.com>
2021-05-22 07:11:57 +0545

    typescript started

---
+++ Typescript/main.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+var myName = function (a) {
+    console.log("The fucnking name was " + a);
+};
+myName('dummy extensio');
---
+++ Typescript/main.ts
@@ -0,0 +1,7 @@
+export {}
+
+let myName =(a)=> {
+    console.log(`The fucnking name was ${a}`)
+}
+
+myName('dummy extensio')
