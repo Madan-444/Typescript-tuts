3889353f116777b635b01ce4ca0694ce5d1478c5
Madan-444 <mmadan3600@gmail.com>
2021-05-22 15:26:33 +0545

    simple changes with interfaces

--- Typescript/main.js
+++ Typescript/main.js
@@ -1,6 +1,38 @@
 "use strict";
 exports.__esModule = true;
-var myName = function (a) {
-    console.log("The fucnking name was " + a);
+var isBeginner = true;
+// let total: number = 0;
+var name = 'Madan Rijal';
+// console.log(`I am ${name} and i have${total} money`)
+// enum variable types
+var Color;
+(function (Color) {
+    Color[Color["red"] = 1] = "red";
+    Color[Color["blue"] = 2] = "blue";
+    Color[Color["purple"] = 3] = "purple";
+    Color[Color["green"] = 10] = "green";
+})(Color || (Color = {}));
+;
+var c = Color.green;
+console.log("my color is:", c);
+function fullName(person) {
+    console.log(person.firstName + " " + person.lastName);
+    console.log("the my age", person.age);
+}
+var obj = {
+    firstName: "Madn Rijal ",
+    lastName: "fuckin boy",
+    age: 89
 };
-myName('dummy extensio');
+fullName(obj);
+// let count = 'sohans';
+// count = 'madan did you mean Madan'
+// console.log("The name",count)
+var bbt = 20;
+var myTotal = function (a) {
+    var total = a + bbt;
+    console.log("The total is", total);
+};
+console.log("The bbt outside the function", bbt);
+myTotal(10);
+// console.log("M,y total value",total)
--- Typescript/main.ts
+++ Typescript/main.ts
@@ -1,7 +1,48 @@
 export {}
 
-let myName =(a)=> {
-    console.log(`The fucnking name was ${a}`)
+let isBeginner: boolean = true;
+// let total: number = 0;
+let name: string = 'Madan Rijal'
+
+// console.log(`I am ${name} and i have${total} money`)
+
+// enum variable types
+
+enum Color {red=1,blue,purple,green=10};
+
+let c:Color = Color.green
+console.log("my color is:",c)
+// We can specify an object as type in typescritp
+interface Person {
+    firstName: string,
+    lastName: string,
+    age: number,
+
 }
 
-myName('dummy extensio')
+function fullName(person: Person) {
+    console.log(`${person.firstName} ${person.lastName}`)
+    console.log("the my age",person.age)
+}
+
+let obj = {
+    firstName: "Madn Rijal ",
+    lastName: "fuckin boy",
+    age: 89
+}
+
+fullName(obj)
+
+
+// let count = 'sohans';
+// count = 'madan did you mean Madan'
+// console.log("The name",count)
+let bbt = 20;
+const myTotal = function(a) {
+
+    const total = a + bbt
+    console.log("The total is",total)
+}
+console.log("The bbt outside the function",bbt)
+myTotal(10)
+// console.log("M,y total value",total)
